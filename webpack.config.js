054f63f18ad0e789dec401b1d98543773d238d58
lee <ajaijs1103@naver.com>
2022-05-17 16:34:37 +0900

    fix: webpack 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
-//import
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
-// 결과물(번들)을 반환하는 설정
 module.exports = {
 	mode: 'development',
 	entry: {
-		main: './js/main.js', // 파일을 읽어드리는 진입점을 설정 
+		main: './js/main.js',
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'), // resolve() : 매개변수 2가지의 경로를 합친다, __dirname : 현재 파일의 경로 , 기본값 => dist 경로
-		filename: 'main.js', // 반환하는 파일의 이름 설정, 기본값 => entry의 경로값
-		clean: true // 기존 webpack 결과물들을 제거하고 새로 결과물을 반환 
+		path: path.resolve(__dirname, 'dist'), 
+		filename: 'main.js', 
+		clean: true 
 	}, 
-	// scss 매칭 모듈
 	module: {
 		rules: [
 			{
@@ -34,22 +31,17 @@ module.exports = {
 			}
 		]
 	},
-
-	// 번들링 후 결과물의 처리 방식 등 다양한 플러그인들 설정
 	plugins: [
 		new HtmlPlugin({
 			template: './index.html'
 		}),
-		// static 폴더안에 모든 것들을 결과물인 dist 폴더에 복사해주는 플러그인 
 		new CopyPlugin({
 			patterns: [
 				{ from: 'static' },
-				{ from: 'scss' , to: 'scss'},
 			]
 		}),
 		new Dotenv({systemvars: true})
 	],
-
 	devServer: {
 		host: 'localhost',
 		port: 8079,
